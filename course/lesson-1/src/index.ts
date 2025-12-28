7adb59584f952d441cee0faf29ce7ba08db45929
Soldier-Hacker <2564563208@qq.com>
2025-12-29 01:25:45 +0800

    1924

--- course/lesson-1/src/index.ts
+++ course/lesson-1/src/index.ts
@@ -2,9 +2,9 @@ import { ethers } from "ethers";
 
 async function main() {
     // faucet https://faucet.polkadot.io/ Paseo AssetHub
-    const URL = "https://services.polkadothub-rpc.com/testnet";
+    const URL = "https://testnet-passet-hub-eth-rpc.polkadot.io";
     const provider = new ethers.JsonRpcProvider(URL);
-    const address = "0x"; // put your address here
+    const address = "0xC3E24Ce27d19DD21EB69d299e69B52A4be7B0A62"; // put your address here
     const balance = await provider.getBalance(address);
     console.log(`Balance of ${address}: ${ethers.formatEther(balance)} ETH`);
 }
